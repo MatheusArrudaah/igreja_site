93692e6733cc2d0fefb0ea9c6971ebc1667c9592
MatheusArrudaah <142322572+MatheusArrudaah@users.noreply.github.com>
2025-06-25 17:23:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
+const openMenu = document.getElementById('open-menu');
+const closeMenu = document.getElementById('close-menu');
+const sidebar = document.getElementById('sidebar');
 
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
+openMenu.addEventListener('click', () => {
+    sidebar.classList.add('show');
+});
+
+closeMenu.addEventListener('click', () => {
+    sidebar.classList.remove('show');
+});
+
+// Fecha o menu se clicar fora dele
+document.addEventListener('click', (e) => {
+    if (!sidebar.contains(e.target) && !openMenu.contains(e.target)) {
+        sidebar.classList.remove('show');
+    }
 });
