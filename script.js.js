f98d86f3ffc136b22c615cb1f5fb2d2cd8f6d1d2
MatheusArrudaah <142322572+MatheusArrudaah@users.noreply.github.com>
2025-06-26 08:50:29 -0300

    Add files via upload

---
+++ script.js.js
@@ -0,0 +1,18 @@
+const openMenu = document.getElementById('open-menu');
+const closeMenu = document.getElementById('close-menu');
+const sidebar = document.getElementById('sidebar');
+
+openMenu.addEventListener('click', () => {
+    sidebar.classList.add('show');
+});
+
+closeMenu.addEventListener('click', () => {
+    sidebar.classList.remove('show');
+});
+
+// Fecha o menu se clicar fora dele
+document.addEventListener('click', (e) => {
+    if (!sidebar.contains(e.target) && !openMenu.contains(e.target)) {
+        sidebar.classList.remove('show');
+    }
+});
